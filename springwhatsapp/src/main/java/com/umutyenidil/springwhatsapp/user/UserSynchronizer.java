7c596957a7426e479b90c50969ab002cac9e7251
Umut Yenidil <umut.yenidil9@gmail.com>
2025-02-11 15:01:34 +0300

    user domain icin authorization sync islemi tamamlandi

---
+++ springwhatsapp/src/main/java/com/umutyenidil/springwhatsapp/user/UserSynchronizer.java
@@ -0,0 +1,42 @@
+package com.umutyenidil.springwhatsapp.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserSynchronizer {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public void synchronizeWithIdp(Jwt token) {
+        log.info("Synchronizing user with idp");
+
+        getUserEmail(token).ifPresent(userEmail -> {
+            log.info("Synchronizing user having email {}", userEmail);
+//            Optional<User> optionalUser = userRepository.findByEmail(userEmail);
+
+            User user = userMapper.fromTokenAttributes(token.getClaims());
+
+//            optionalUser.ifPresent(value -> user.setId(optionalUser.get().getId()));
+
+            userRepository.save(user);
+        });
+    }
+
+    private Optional<String> getUserEmail(Jwt token) {
+        Map<String, Object> attributes = token.getClaims();
+
+        if (attributes.containsKey("email")) {
+            return Optional.of(attributes.get("email").toString());
+        }
+
+        return Optional.empty();
+    }
+}
