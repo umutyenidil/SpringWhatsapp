6d0b3f4143fb943125e93dd683fe394865c24f2f
Umut Yenidil <umut.yenidil9@gmail.com>
2025-02-11 17:43:40 +0300

    user domain kullanicilari getirecek sekilde guncellendi

--- springwhatsapp/src/main/java/com/umutyenidil/springwhatsapp/user/UserRepository.java
+++ springwhatsapp/src/main/java/com/umutyenidil/springwhatsapp/user/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
@@ -20,5 +22,12 @@ public interface UserRepository extends JpaRepository<User, String> {
     )
     Optional<User> findByPublicId(
             @Param("publicId") String publicId
+    );
+
+    @Query(
+            name = UserConstants.FIND_ALL_USERS_EXCEPT_SELF
+    )
+    List<User> findAllUsersExceptSelf(
+            @Param("publicId") String senderId
     );
 }
